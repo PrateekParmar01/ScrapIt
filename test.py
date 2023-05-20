10df8f473c91681f77abebffd1c327a240608b9f
Saarthak Verma <lci2021035@iiitl.ac.in>
2023-05-21 05:28:53 +0530

    fixed indeed.py

--- test.py
+++ test.py
@@ -3,8 +3,6 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium_stealth import stealth
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import warnings
 import csv
 
@@ -37,13 +35,7 @@ stealth(
     fix_hairline=True,
 )
 driver.get(url)
-try:
-    WebDriverWait(driver, 60).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "nav#gnav-main-container"))
-    )
-except:
-    print("Connection slow; Timed out")
-    exit()
+
 script = """
 window.scrollTo(0, document.body.scrollHeight);
 """
@@ -55,72 +47,51 @@ job_cards = driver.find_elements(
 job_links = []
 jobs_page_source = []
 # job_cards = job_cards[:4]
-for card in job_cards:
-    card.click()
-    sleep(1)
-    driver.switch_to.window(driver.window_handles[-1])
-    sleep(1)
-    try:
-        WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located(
-                (By.CSS_SELECTOR, "div.jobsearch-CompanyInfoContainer")
-            )
-        )
+filename = filename = f"{query} jobs in {location} indeed.csv"
+seen=set()
+with open(filename, "w", newline='') as myfile:
+    spamWriter = csv.writer(myfile, dialect='excel')
+    spamWriter.writerow(["Job Title","Company","Salary","URL","Job type"])
+    for card in job_cards:
+        card.click()
+        sleep(1)
+        driver.switch_to.window(driver.window_handles[1])
         job_links.append(driver.current_url)
         jobs_page_source.append(driver.page_source)
-    except:
-        pass
-    finally:
         driver.close()
-        sleep(1)
         driver.switch_to.window(driver.window_handles[0])
         sleep(1)
 
-driver.quit()
+    driver.quit()
+    entries = []
 
-filename = filename = f"{query} jobs in {location} indeed.csv"
-myfile = open(filename, "w", newline="")
-spamWriter = csv.writer(myfile, dialect="excel")
-
-header = ["Job Title", "Company", "Salary", "URL"]
-spamWriter.writerow(header)
-
-
-entries = []
-for i in range(0, len(jobs_page_source)):
-    page = jobs_page_source[i]
-    url = job_links[i]
-    soup = BeautifulSoup(page, "html.parser")
-    try:
-        details = soup.find("div", id="jobDetailsSection")
-        details = details.children  # type: ignore
-        details = [x for x in details][1:]
-        entry = {}
-        companyinfo = soup.find("div", {"data-company-name": "true"}).text
-        entry["Company"] = companyinfo
-        entry["URL"] = url
-        # l=["WEB DEVELOPER","NA","NA","NA","NA"]
-        for detail in details:
-            try:
+    for i in range(0, len(jobs_page_source)):
+        page = jobs_page_source[i]
+        url = job_links[i]
+        soup = BeautifulSoup(page, "html.parser")
+        try:
+            details = soup.find("div", id="jobDetailsSection")
+            details = details.children  # type: ignore
+            details = [x for x in details][1:]
+            entry = {}
+            companyinfo = soup.find("div",class_="jobsearch-CompanyInfoContainer").text
+            entry["Company"] = companyinfo
+            entry["URL"] = url
+            l=["WEB DEVELOPER","NA","NA","NA","NA"]
+            l[1]=companyinfo
+            l[3]=url
+            for detail in details:
                 field, value = [x for x in detail.children]  # type: ignore
                 field = field.text
                 value = value.text
+                if(field=="Job type"):
+                    l[4]=value
+                else:
+                    l[2]=value
                 entry[field] = value
-            except:
-                entry = {}
-                break
-        if entry:
             entries.append(entry)
-    except Exception as e:
-        print(e)
-
-for entry in entries:
-    l = ["Web Developer"]
-    for column in header[1:]:
-        try:
-            l.append(entry[column])
-        except:
-            l.append("NA")
-    spamWriter.writerow(l)
-
-myfile.close()
+            spamWriter.writerow(l)
+            seen.add(l[1])
+        except Exception as e:
+            print(e)
+print(entries)
